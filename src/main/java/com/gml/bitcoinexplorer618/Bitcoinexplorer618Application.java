120b7ed02d7966f700d784f7acbcbf962e99cea4
木木 <295623507@qq.com>
2019-06-18 10:56:33 +0800

    first

---
+++ src/main/java/com/gml/bitcoinexplorer618/Bitcoinexplorer618Application.java
@@ -0,0 +1,13 @@
+package com.gml.bitcoinexplorer618;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bitcoinexplorer618Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bitcoinexplorer618Application.class, args);
+    }
+
+}
